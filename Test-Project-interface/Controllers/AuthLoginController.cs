1cecaa47fe1d6ea8ae408b8f0d218c08e3d2f54a
thaott11 <luuvanthao524@gmail.com>
2024-12-14 13:50:33 +0700

    login

---
+++ Test-Project-interface/Controllers/AuthLoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Project_interface.Controllers
+{
+    public class AuthLoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPass()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
