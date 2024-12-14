627a547ccbbbb22d43adeb71eabe7a066213620c
thaott11 <luuvanthao524@gmail.com>
2024-12-14 11:00:50 +0700

    admin

---
+++ Test-Project-interface/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Project_interface.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult User()
+        {
+            return View();
+        }
+    }
+}
